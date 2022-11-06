16e41cdf8824c63effe28de964dafadc20abcec8
introtoxicXXl <minhajulabedin648@gmail.com>
2022-11-06 12:46:48 +0600

    create compoment

---
+++ src/compoment/Verification/Verification.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Verification = () => {
+  return (
+    <div>
+      <h1>This is Verification</h1>
+    </div>
+  );
+};
+
+export default Verification;
---
+++ src/compoment/Wngs/Wings.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Wings = () => {
+  return (
+    <div>
+      <h1>Ti=his is Wings</h1>
+    </div>
+  );
+};
+
+export default Wings;
